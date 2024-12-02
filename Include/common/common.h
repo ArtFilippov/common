af41b1ee04bc28b811ad9e3e2f84ef740c9766d8
Artem Filippov <filippar@trp-group.local>
2024-12-02 13:11:51 +0300

    first commit:

---
+++ Include/common/common.h
@@ -0,0 +1,58 @@
+#pragma once
+#include <unistd.h>
+#include <chrono>
+#include <sstream>
+
+namespace common {
+int writeAll(int fd, uint8_t *buf, int len);
+int readAll(int fd, uint8_t *buf, int len);
+int readUntil(int fd, uint8_t *buf, int len, uint64_t time);
+std::stringstream print(uint8_t *pac, int len);
+uint64_t timeMs();
+
+template <typename Int> void setBigEndian(uint8_t *pac, Int x) {
+    for (int i = sizeof(x) - 1; i >= 0; --i) {
+        pac[i] = x & 0xFF;
+        x >>= __CHAR_BIT__;
+    }
+}
+
+template <typename Int> Int getBigEndian(uint8_t *pac) {
+    Int ans = 0;
+    for (int i = 0; i < sizeof(ans); ++i) {
+        ans <<= __CHAR_BIT__;
+        ans += pac[i];
+    }
+
+    return ans;
+}
+
+template <typename Int> void setLittleEndian(uint8_t *pac, Int x) {
+    for (int i = 0; i < sizeof(x); ++i) {
+        pac[i] = x & 0xFF;
+        x >>= __CHAR_BIT__;
+    }
+}
+
+template <typename Int> Int getLittleEndian(uint8_t *pac) {
+    Int ans = 0;
+    for (int i = sizeof(ans) - 1; i >= 0; --i) {
+        ans <<= __CHAR_BIT__;
+        ans += pac[i];
+    }
+
+    return ans;
+}
+
+template <typename Int> Int toGrayCode(Int x) {
+    Int ans = 0;
+    for (int i = 0; i < sizeof(x); ++i) {
+        uint8_t byte = x & 0xFF;
+        uint8_t grey = byte ^ (byte >> 1);
+        ans |= grey << (i * __CHAR_BIT__);
+        x >>= __CHAR_BIT__;
+    }
+
+    return ans;
+}
+} // namespace common
---
+++ src/common.cpp
@@ -0,0 +1,64 @@
+#include "common/common.h"
+
+int common::writeAll(int fd, uint8_t *buf, int len) {
+    int total = 0;
+    int n{};
+
+    while (total < len) {
+        n = write(fd, buf + total, len - total);
+        if (n == -1 || n == 0) {
+            break;
+        }
+        total += n;
+    }
+
+    return n == -1 ? -1 : total;
+}
+
+int common::readAll(int fd, uint8_t *buf, int len) {
+    int total = 0;
+    int n{};
+
+    while (total < len) {
+        n = read(fd, buf + total, len - total);
+        if (n == -1 || n == 0) {
+            break;
+        }
+        total += n;
+    }
+
+    return n == -1 ? -1 : total;
+}
+
+int common::readUntil(int fd, uint8_t *buf, int len, uint64_t time) {
+    int total = 0;
+    int n{};
+
+    while (total < len &&
+           std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch())
+                   .count() < time) {
+        n = read(fd, buf + total, len - total);
+        if (n == -1) {
+            break;
+        }
+        total += n;
+    }
+
+    return n == -1 ? -1 : total;
+}
+
+uint64_t common::timeMs() {
+    return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch())
+        .count();
+}
+
+std::stringstream common::print(uint8_t *pac, int len) {
+    std::stringstream buf;
+    buf << "[" << std::hex;
+    for (int i = 0; i < len - 1; ++i) {
+        buf << "0x" << (int) pac[i] << ", ";
+    }
+    buf << "0x" << (int) pac[len - 1] << "]";
+
+    return buf;
+}
