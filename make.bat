f75d03ae9f3d7e854b3296d7c6229e47c874cf4b
Artem Filippov <filippar@trp-group.local>
2024-12-12 14:47:20 +0300

    fix: make

--- make.bat
+++ make.bat
@@ -8,6 +8,6 @@ if exist %~dp0build\ (
     RD /S "%~dp0build\"
 )
 
-cmake %~dp0 -B %~dp0build -G "MinGW Makefiles" -DOS=arm -DCMAKE_PREFIX_PATH="%workspaceDirectory%"
-cmake %~dp0 --build %~dp0build
-cmake %~dp0 --install %~dp0build --prefix %workspaceDirectory%
+cmake -S %~dp0 -B %~dp0build -G "MinGW Makefiles" -DOS=arm -DCMAKE_PREFIX_PATH="%workspaceDirectory%"
+cmake -S %~dp0 --build %~dp0build
+cmake -S %~dp0 --install %~dp0build --prefix %workspaceDirectory%
