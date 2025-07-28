cc81260338e2e3e8c1efc616a3f7d5b9ad46d626
Artem Filippov <filippar@trp-group.local>
2025-07-28 16:57:02 +0300

    feat: написал несколько реализаций lockable<T>

--- Include/common/common_templates.h
+++ Include/common/common_templates.h
@@ -2,6 +2,7 @@
 #include <atomic>
 #include <exception>
 #include <iostream>
+#include <memory>
 
 namespace common {
 template <typename T> class array_atomic {
@@ -92,7 +93,7 @@ template <typename Int> void unsetAtomicBits(std::atomic<Int> &atomic, Int bits)
 }
 
 template <typename Ptr> class no_null_ptr {
-    Ptr p;
+    Ptr p_;
 
   public:
     no_null_ptr(Ptr p) {
@@ -100,7 +101,114 @@ template <typename Ptr> class no_null_ptr {
             std::cerr << "terminate after providing nullptr in no_null_ptr constructor" << std::endl;
             std::terminate();
         }
+
+        p_ = p;
     }
+
+    Ptr point() { return p_; }
+};
+
+template <typename _Tp, typename... _Args>
+inline no_null_ptr<std::shared_ptr<_Tp>> make_no_null_shared_ptr(_Args &&...__args) {
+
+    auto ptr = std::make_shared<_Tp>(std::forward<_Args>(__args)...);
+
+    return no_null_ptr(ptr);
+}
+
+template <typename T> class lockable {
+  public:
+    virtual ~lockable() = default;
+
+    void store(no_null_ptr<std::shared_ptr<T>> ptr) { store(ptr.point()); }
+
+    void store(const T *ptr) { store(std::make_shared<const T>(no_null_ptr<const T *>(ptr).point())); }
+
+    virtual std::shared_ptr<const T> lock() = 0;
+
+    virtual void store(std::shared_ptr<T> inst) = 0;
+};
+
+template <typename T> class lockable_ptr : public lockable<T> {
+
+    std::atomic<std::shared_ptr<const T>> p;
+
+  public:
+    lockable_ptr(no_null_ptr<std::shared_ptr<T>> instance) : p(instance.point()) {}
+
+    std::shared_ptr<const T> lock() override { return p.load(std::memory_order_acquire); }
+
+    void store(std::shared_ptr<T> newPtr) override {
+        p.store(common::no_null_ptr(newPtr)->point(), std::memory_order_release);
+    }
+};
+
+template <typename T> class lockfree_ptr : public lockable<T> {
+
+    std::shared_ptr<T> inst1;
+    std::shared_ptr<T> inst2;
+    std::atomic<std::shared_ptr<T> *> switchTo;
+    std::atomic<int> calls{0};
+    std::atomic<int> storeCalls{0};
+
+    std::atomic<std::shared_ptr<T> *> p;
+    std::atomic_bool rightToModifyInsts{true};
+
+  public:
+    lockfree_ptr(no_null_ptr<std::shared_ptr<T>> instance) : inst1(instance.point()), inst2(instance.point()) {
+        p.store(&inst1);
+        switchTo.store(&inst2);
+    }
+
+    std::shared_ptr<const T> lock() override {
+        calls.fetch_add(1, std::memory_order_acq_rel);
+        std::shared_ptr<T> *myPtr = p.load(std::memory_order_acquire);
+        std::shared_ptr<T> result = std::shared_ptr<T>(*myPtr);
+        lastCallUpdateInstPtr();
+        return result;
+    }
+
+    void store(std::shared_ptr<T> newPtr) override {
+        calls.fetch_add(1, std::memory_order_acq_rel);
+        storeCalls.fetch_add(1, std::memory_order_acq_rel);
+
+        if (p.load(std::memory_order_acquire) == &inst1) {
+            switchTo.store(&inst2, std::memory_order_release);
+        } else {
+            switchTo.store(&inst1, std::memory_order_release);
+        }
+
+        lastStoreCallUpdateInst(newPtr);
+        lastCallUpdateInstPtr();
+    }
+
+  private:
+    void lastCallUpdateInstPtr() {
+        std::shared_ptr<T> *switchFrom = (switchTo.load(std::memory_order_acquire) == &inst1) ? &inst2 : &inst1;
+        if (calls.fetch_sub(1, std::memory_order_acq_rel) == 1) {
+            p.compare_exchange_strong(switchFrom, switchTo.load(std::memory_order_acquire), std::memory_order_acq_rel,
+                                      std::memory_order_relaxed);
+        }
+    }
+
+    void lastStoreCallUpdateInst(std::shared_ptr<T> newPtr) {
+        if (storeCalls.fetch_sub(1, std::memory_order_acq_rel) == 1) {
+            bool rightToModify = true;
+            if (rightToModifyInsts.compare_exchange_strong(rightToModify, false, std::memory_order_acq_rel,
+                                                           std::memory_order_relaxed)) {
+                *switchTo = newPtr;
+                rightToModifyInsts.store(true, std::memory_order_release);
+            }
+        }
+    }
+};
+
+template <typename _Tp, typename... _Args> inline std::shared_ptr<lockable<_Tp>> make_lockable_ptr(_Args &&...__args) {
+
+    std::shared_ptr<_Tp> ptr = std::make_shared<_Tp>(std::forward<_Args>(__args)...);
+    no_null_ptr<std::shared_ptr<_Tp>> nnp = no_null_ptr(ptr);
+
+    return std::make_shared<lockfree_ptr<_Tp>>(nnp);
 };
 
 } // namespace common
